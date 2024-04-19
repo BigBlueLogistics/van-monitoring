ad7b1e178bb76b69baecb53fb94f016c36156402
Roelan <roelaneroy@gmail.com>
2024-04-19 11:33:02 +0800

    implement forwardRef in MDImage

--- app/src/components/atoms/MDImage/index.tsx
+++ app/src/components/atoms/MDImage/index.tsx
@@ -1,11 +1,23 @@
+import { forwardRef } from 'react';
 import Image from 'next/image';
 import MDBox from '@/components/atoms/MDBox';
 import { TMDImageIcon } from './types';
 
-function MDImage({ src, alt = '', width = 24, height = 24, sx, ...rest }: TMDImageIcon) {
-  return (
-    <MDBox component={Image} src={src} alt={alt} width={width} height={height} sx={sx} {...rest} />
-  );
-}
+const MDImage = forwardRef<HTMLImageElement, TMDImageIcon>(
+  ({ width, height, alt = '', src, sx, ...rest }, ref) => (
+    <MDBox
+      ref={ref}
+      component={Image}
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      sx={sx}
+      {...rest}
+    />
+  )
+);
+
+MDImage.displayName = 'MDImage';
 
 export default MDImage;
